bf76ce10c5e09f7194673680a72e983681ffa7e0
ABDUL RAHAMAN <36rahaman@gmail.com>
2024-01-02 15:17:18 +0530

    chore: used swiper for carousal

--- src/app/components/caurosal.jsx
+++ src/app/components/caurosal.jsx
@@ -1,44 +1,75 @@
-import React, { useState } from 'react'
-import SimpleImageSlider from "react-simple-image-slider";
-import { useQuery } from '@tanstack/react-query';
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "../../../public/style.css";
+
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+
+import { useQuery } from "@tanstack/react-query";
 
 function Caurosal() {
-  const [visible,setvisible]=useState(false);
+  const [visible, setvisible] = useState(false);
   const fetchProductList = async () => {
     const response = await fetch("https://fakestoreapi.com/products");
     return response.json();
   };
-  const {
-    isLoading,
-    data,
-    
-  } = useQuery({
+  const { isLoading, data } = useQuery({
     queryKey: ["productdata"],
     queryFn: fetchProductList,
   });
 
   if (isLoading) return <p>Loading........</p>;
-  
+
   return (
-    <div className='mx'>
-      <div className='ml-52 border-primary' onMouseLeave={()=>{setvisible(false)}} onMouseEnter={()=>{setvisible(true)}}>
-      
-      <SimpleImageSlider
-        navStyle={2}
-        width={800}
-        height={500}
-        images={data?.slice(0,15).map((ele,id)=>{return {url:ele.image}})}
-        showBullets={true}
-        autoPlay={true}
-        bgColor={"primary"}
-        showNavs={visible}
-      />
-      HII
+    <div className="mb-20">
+      <div
+        className="mx-72 w-92 h-42 "
+        onMouseLeave={() => {
+          setvisible(false);
+        }}
+        onMouseEnter={() => {
+          setvisible(true);
+        }}
+      >
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+            delay: 1500,
+            disableOnInteraction: true,
+          }}
+          pagination={{
+          dynamicBullets: true,
+        }}
+          loop={true}
+          navigation={visible}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          {data?.slice(0, 4).map((ele, id) => {
+            return (
+              <li key={id} onMouseEnter={()=>{setvisible(true)}} onMouseLeave={()=>{setvisible(false)}}>
+                <SwiperSlide>
+                  <div className="flex">
+
+                    <p className="place-self-center">{ele.title}</p>
+                    <div className="w-[60vw] h-[90vh]">
+                    <img className="object-cover" src={ele.image} />
+                    </div>
+                  </div>
+                </SwiperSlide>
+              </li>
+            );
+          })}
+        </Swiper>
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default Caurosal
+export default Caurosal;
